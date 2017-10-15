084b7fc46166a062dbfb1357050ad289bc6831ba
albertbahia <albert.bahia1@gmail.com>
2017-10-15 16:44:41 -0400

    moved api call from controller to Ticker model method

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -24,58 +24,7 @@ router.get('/', function (req, res, next) {
 // Phase 2: Modify this route so that it automatically makes the request on intervals 
 // ---------------------------------------------------------------
 router.get('/get-tickers', function(req, res, next) {
-  fetch('https://api.coinmarketcap.com/v1/ticker/')
-    .then(function(res) {
-      return res.json()
-    }).then(function(json) {
-
-      function numberWithCommas(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-      }
-    
-      var millionthMktCaps = [],
-        tickers = [],
-        headers = Object.keys(json[0]),
-        additionalHeaders = ['Millionth USD', 'Millionth of Coins','% To Buy (Top 10)'];
-		  var totalTopTenUSD = 0,
-					totalTopTwentyUSD = 0;
-			
-			var currency,
-					number;
-
-      additionalHeaders.forEach(function(el) {
-        headers.push(el);
-      });
-
-      // ---------------------------------------------------------------
-      // Calculation Millionth of Supply from CoinMarketCap API Response
-      // ---------------------------------------------------------------
-      json.forEach(function(el) {
-        
-        el.millionthMktCapUSD = "$" + String(numberWithCommas((el.market_cap_usd * Math.pow(10, -6)).toFixed(2)));
-
-        el.millionthCoins = (el.available_supply * Math.pow(10, -6)).toFixed(1);
-
-        tickers.push(el);
-      });
-      // ---------------------------------------------------------------
-
-			// ---------------------------------------------------------------
-			// Calculate Percentage (of your available disposable income) to Buy of the Top 10 Coins
- 			// ---------------------------------------------------------------
-			for (let i = 0; i < 10; i++) {
-				currency = tickers[i].millionthMktCapUSD;
-				number = Number(currency.replace(/[^0-9\.-]+/g,""));
-								
-				tickers[i].topTenBuyUSD = number; // Set up the numerator for division
-				totalTopTenUSD += tickers[i].topTenBuyUSD; // Add the top 10 coins millionth market cap in USD
-
-			}
-
-			for (let j = 0; j < 10; j++ ) {
-				tickers[j].topTenBuyPerc = ((tickers[j].topTenBuyUSD / totalTopTenUSD) * 100).toFixed(2) + "%";
-			}
-			// ---------------------------------------------------------------
+  
 
 			// ------------------------
       // Render in index template
--- app/models/ticker.js
+++ app/models/ticker.js
@@ -17,4 +17,59 @@ var Ticker = thinky.createModel('Ticker', {
    percent_change_7d: String
 });
 
+Ticker.define("getTickers", function() {
+   fetch('https://api.coinmarketcap.com/v1/ticker/')
+   .then(function(res) {
+     return res.json()
+   }).then(function(json) {
+
+     function numberWithCommas(x) {
+       return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+     }
+   
+     var millionthMktCaps = [],
+       tickers = [],
+       headers = Object.keys(json[0]),
+       additionalHeaders = ['Millionth USD', 'Millionth of Coins','% To Buy (Top 10)'];
+       var totalTopTenUSD = 0,
+              totalTopTwentyUSD = 0;
+        
+        var currency,
+              number;
+
+     additionalHeaders.forEach(function(el) {
+       headers.push(el);
+     });
+
+     // ---------------------------------------------------------------
+     // Calculation Millionth of Supply from CoinMarketCap API Response
+     // ---------------------------------------------------------------
+     json.forEach(function(el) {
+       
+       el.millionthMktCapUSD = "$" + String(numberWithCommas((el.market_cap_usd * Math.pow(10, -6)).toFixed(2)));
+
+       el.millionthCoins = (el.available_supply * Math.pow(10, -6)).toFixed(1);
+
+       tickers.push(el);
+     });
+     // ---------------------------------------------------------------
+
+        // ---------------------------------------------------------------
+        // Calculate Percentage (of your available disposable income) to Buy of the Top 10 Coins
+         // ---------------------------------------------------------------
+        for (let i = 0; i < 10; i++) {
+           currency = tickers[i].millionthMktCapUSD;
+           number = Number(currency.replace(/[^0-9\.-]+/g,""));
+                       
+           tickers[i].topTenBuyUSD = number; // Set up the numerator for division
+           totalTopTenUSD += tickers[i].topTenBuyUSD; // Add the top 10 coins millionth market cap in USD
+
+        }
+
+        for (let j = 0; j < 10; j++ ) {
+           tickers[j].topTenBuyPerc = ((tickers[j].topTenBuyUSD / totalTopTenUSD) * 100).toFixed(2) + "%";
+        }
+        // ---------------------------------------------------------------
+});
+
 module.exports = Ticker;
