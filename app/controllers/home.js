0363d691c11e6a943dd4bf1342a603874dd4132e
albertbahia <albert.bahia1@gmail.com>
2017-10-15 20:00:01 -0400

    deleted old code

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -9,21 +9,12 @@ module.exports = function (app) {
   app.use('/', router);
 };
 
-router.get('/', function (req, res, next) {
-  Article.run().then(function (articles) {
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
-    });
-  });
-});
-
 // ---------------------------------------------------------------
 // Route for GETting the Ticker data from the Coinmarketcap API.
 // Phase 1: Manually make API Call with this route to get the ticker data.
 // Phase 2: Modify this route so that it automatically makes the request on intervals 
 // ---------------------------------------------------------------
-router.get('/get-tickers', function(req, res, next) {
+router.get('/', function(req, res, next) {
   var coin = new Ticker({});
   
   coin.getTickers().then(function(result) {
